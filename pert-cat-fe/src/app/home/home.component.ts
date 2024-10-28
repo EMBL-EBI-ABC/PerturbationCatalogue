ac64006c161405e51b1a9d241da35f6fa9aa994e
Alexey Sokolov <alexey@ebi.ac.uk>
2024-10-28 13:55:29 +0000

    Adding header, footer and placeholder text to the home page

--- pert-cat-fe/src/app/home/home.component.ts
+++ pert-cat-fe/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "../shared/header/header.component";
+import {MatCardModule} from "@angular/material/card";
+import {FooterComponent} from "../shared/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
