4d712f875242430ca30d22cbb2564e89cb7285f1
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2024-11-04 14:01:46 +0000

    Original quick implementation for BigQuery

--- pert-cat-fe/src/app/home/home.component.ts
+++ pert-cat-fe/src/app/home/home.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {HeaderComponent} from "../shared/header/header.component";
 import {MatCardModule} from "@angular/material/card";
 import {FooterComponent} from "../shared/footer/footer.component";
+import { BigQueryService } from '../bigquery.service';
+import { MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, MatCardModule],
+  imports: [HeaderComponent, FooterComponent, MatCardModule, MatTableModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  data: any[] = [];
 
+  constructor(private bigQueryService: BigQueryService) { }
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.bigQueryService.getData().subscribe({
+      next: (response) => {
+        this.data = response.rows;
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      }
+    });
+  }
 }
