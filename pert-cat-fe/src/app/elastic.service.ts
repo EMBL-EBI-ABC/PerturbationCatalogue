94188c81cd723e9fc44d3e3faad08ab18692c6b2
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2024-11-11 23:30:05 +0000

    Implement pagination

--- pert-cat-fe/src/app/elastic.service.ts
+++ pert-cat-fe/src/app/elastic.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
 
 @Injectable({
   providedIn: 'root',
@@ -10,16 +9,15 @@ import { map } from 'rxjs/operators';
 export class ElasticService {
   private readonly fastApiUrl = 'https://pert-cat-be-959149465821.europe-west2.run.app/search';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getData(): Observable<any> {
-
-    // Temporary substitution with first 10 entries from GitHub.
-    const response = this.http.get(this.fastApiUrl)
-
-    // Return the fields we need.
-    return response.pipe(
-      map((response: any) => response.results)
+  getData(start: number = 0, size: number = 10): Observable<any> {
+    const params = { start: start.toString(), size: size.toString() };
+    return this.http.get(this.fastApiUrl, { params }).pipe(
+      map((response: any) => ({
+        results: response.results,
+        total: response.total
+      }))
     );
   }
 }
--- pert-cat-fe/src/app/home/home.component.ts
+++ pert-cat-fe/src/app/home/home.component.ts
@@ -4,23 +4,27 @@ import { MatCardModule } from "@angular/material/card";
 import { FooterComponent } from "../shared/footer/footer.component";
 import { ElasticService } from '../elastic.service';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { CommonModule } from '@angular/common';
+
+
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, MatCardModule, MatTableModule, CommonModule],
+  imports: [HeaderComponent, FooterComponent, MatCardModule, MatTableModule, MatPaginator, MatPaginatorModule, CommonModule,
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
   data: any;
+  totalResults = 0;
+  pageSize = 10;
+  currentPage = 0;
 
-  constructor(
-    private elasticService: ElasticService,
-  ) { };
-
+  constructor(private elasticService: ElasticService) {}
 
   ngOnInit() {
     this.fetchData();
@@ -31,14 +35,23 @@ export class HomeComponent implements OnInit {
   }
 
   fetchData() {
-    this.elasticService.getData().subscribe(
+    const start = this.currentPage * this.pageSize;
+    const size = this.pageSize;
+    this.elasticService.getData(start, size).subscribe(
       (response) => {
-        this.data = response;
-        console.log('Data received:', this.data.length);
+        this.data = response.results;
+        this.totalResults = response.total;
       },
       (error) => {
         console.error('Error fetching data:', error);
       }
     );
   }
+
+  onPageChange(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    this.fetchData();
+  }
+
 }
