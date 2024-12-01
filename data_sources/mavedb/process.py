8077f16069f7496ec7f2552f0337b7ca17543614
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2024-12-01 20:17:39 +0000

    Keep track of all possible target gene cases

--- data_sources/mavedb/process.py
+++ data_sources/mavedb/process.py
@@ -1,6 +1,8 @@
 import argparse
+from collections import Counter
 import json
 import pandas as pd
+
 
 def process_json_to_csv(input_filename, output_filename):
     # Load JSON data from file.
@@ -10,10 +12,28 @@ def process_json_to_csv(input_filename, output_filename):
     # Prepare a list to store the rows.
     rows = []
 
+    # Keep track of the various target gene combinations.
+    target_gene_cnt = Counter()
+
     # Navigate through the JSON structure and extract required fields.
     for experiment_set in data.get("experimentSets", []):
         for experiment in experiment_set.get("experiments", []):
             for score_set in experiment.get("scoreSets", []):
+                # Analyse target genes.
+                target_genes = score_set.get("targetGenes", [])
+                match len(target_genes):
+                    case 0:
+                        target_gene_category = "No target gene"
+                    case 1:
+                        match target_genes[0]["targetSequence"]["taxonomy"]["taxId"]:
+                            case 9606:
+                                target_gene_category = "Single target gene, human"
+                            case _:
+                                target_gene_category = "Single target gene, non-human"
+                    case _:
+                        target_gene_category = "Multiple target genes"
+                # Track the counts of different target gene cases.
+                target_gene_cnt[target_gene_category] += 1
                 # Append row data with selected fields.
                 rows.append({
                     "title": score_set.get("title", ""),
@@ -31,6 +51,10 @@ def process_json_to_csv(input_filename, output_filename):
     with open(output_filename, 'w') as f:
         for row in rows:
             f.write(json.dumps(row) + '\n')
+
+    # Output target gene information.
+    for key, value in target_gene_cnt.most_common():
+        print(key, value)
 
 def main():
     # Set up argument parser.
