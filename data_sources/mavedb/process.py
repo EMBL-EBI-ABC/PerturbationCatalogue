efbda678b62f34d8f28b1d4ecd4d40e9e9f86168
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2024-12-01 20:18:19 +0000

    Improve formatting

--- data_sources/mavedb/process.py
+++ data_sources/mavedb/process.py
@@ -38,36 +38,44 @@ def process_json_to_csv(input_filename, output_filename):
                 if target_gene_category != "Single target gene, human":
                     continue
                 # Append row data with selected fields.
-                rows.append({
-                    "title": score_set.get("title", ""),
-                    "methodText": score_set.get("methodText", ""),
-                    "abstractText": score_set.get("abstractText", ""),
-                    "shortDescription": score_set.get("shortDescription", ""),
-                    "urn": score_set.get("urn", ""),
-                    "numVariants": score_set.get("numVariants", 0)
-                })
+                rows.append(
+                    {
+                        "title": score_set.get("title", ""),
+                        "methodText": score_set.get("methodText", ""),
+                        "abstractText": score_set.get("abstractText", ""),
+                        "shortDescription": score_set.get("shortDescription", ""),
+                        "urn": score_set.get("urn", ""),
+                        "numVariants": score_set.get("numVariants", 0),
+                    }
+                )
 
     # Convert list of rows to a DataFrame.
     df = pd.DataFrame(rows)
 
     # Save DataFrame to JSONL.
-    with open(output_filename, 'w') as f:
+    with open(output_filename, "w") as f:
         for row in rows:
-            f.write(json.dumps(row) + '\n')
+            f.write(json.dumps(row) + "\n")
 
     # Output target gene information.
     for key, value in target_gene_cnt.most_common():
         print(key, value)
 
+
 def main():
     # Set up argument parser.
     parser = argparse.ArgumentParser(description="Process JSON to CSV")
-    parser.add_argument("--input-filename", type=str, required=True, help="Input JSON filename")
-    parser.add_argument("--output-filename", type=str, required=True, help="Output CSV filename")
+    parser.add_argument(
+        "--input-filename", type=str, required=True, help="Input JSON filename"
+    )
+    parser.add_argument(
+        "--output-filename", type=str, required=True, help="Output CSV filename"
+    )
     args = parser.parse_args()
 
     # Process JSON and save to CSV.
     process_json_to_csv(args.input_filename, args.output_filename)
+
 
 if __name__ == "__main__":
     main()
