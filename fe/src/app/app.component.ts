4f2f6a2bc4105a9973abccd60daabf361b6f734b
Alexey Sokolov <alexey@ebi.ac.uk>
2024-12-12 11:39:03 +0000

    Adding dashboards component

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {HeaderComponent} from "./shared/header/header.component";
+import {FooterComponent} from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 export const routes: Routes = [
   { path: 'home', title: 'Perturbation Catalogue | Home',  component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'dashboards', component: DashboardComponent },
 ];
