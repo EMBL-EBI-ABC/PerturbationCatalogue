075dc3fe5a3f4e2ce7dfe3b3eeec09dfca679430
Alexey Sokolov <alexey@ebi.ac.uk>
2024-12-14 10:12:00 +0000

    Adding search on top of MaveDB

--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {SearchComponent} from "./search/search.component";
 
 export const routes: Routes = [
   { path: 'home', title: 'Perturbation Catalogue | Home',  component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'dashboards', component: DashboardComponent },
+  { path: 'dashboards', title: 'Perturbation Catalogue | Dashboards', component: DashboardComponent },
+  { path: 'search', title: 'Perturbation Catalogue | Search', component: SearchComponent },
 ];
