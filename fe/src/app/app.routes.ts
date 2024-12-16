e67f00b77ae234f79334d9beb288f5457bffe3b1
Alexey Sokolov <alexey@ebi.ac.uk>
2024-12-16 14:01:10 +0000

    Refactoring

--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {SearchComponent} from "./search/search.component";
+import {DataPortalComponent} from "./data-portal/data-portal.component";
 
 export const routes: Routes = [
   { path: 'home', title: 'Perturbation Catalogue | Home',  component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'data_portal', title: 'Perturbation Catalogue | Data Portal', component: DataPortalComponent },
   { path: 'dashboards', title: 'Perturbation Catalogue | Dashboards', component: DashboardComponent },
   { path: 'search', title: 'Perturbation Catalogue | Search', component: SearchComponent },
 ];
--- fe/src/app/home/home.component.ts
+++ fe/src/app/home/home.component.ts
@@ -1,104 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { HeaderComponent } from "../shared/header/header.component";
-import { MatCardModule } from "@angular/material/card";
-import { FooterComponent } from "../shared/footer/footer.component";
-import { ElasticService } from '../services/elastic.service';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { marked } from 'marked';
-import {MatList, MatListModule} from "@angular/material/list";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    MatCardModule,
-    MatTableModule,
-    MatPaginator,
-    MatPaginatorModule,
-    CommonModule,
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-    MatListModule,
-  ],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  data: any;
-  aggregations: any;
-  filters = {
-    sequenceType: [],
-    geneCategory: [],
-    publicationYear: []
-  }
-  totalResults = 0;
-  pageSize = 15;
-  currentPage = 0;
-  searchQuery: string = '';
+export class HomeComponent {
 
-  displayedColumns: string[] = ['urn', 'title', 'sequenceType', 'geneName', 'geneCategory', 'publicationYear',
-    'numVariants'];
-
-  constructor(private elasticService: ElasticService) {}
-
-  ngOnInit() {
-    this.fetchData();
-  }
-
-  getKeys(obj: any): string[] {
-    return Object.keys(obj);
-  }
-
-  fetchData() {
-    const start = this.currentPage * this.pageSize;
-    const size = this.pageSize;
-    const query = this.searchQuery;
-    this.elasticService.getData(start, size, query, this.filters).subscribe(
-      (response) => {
-        this.data = response.results.map((row: Record<string, string>) => {
-          const parsedRow: Record<string, string> = {};
-          for (const key of this.getKeys(row)) {
-            parsedRow[key] = row[key];
-          }
-          return parsedRow;
-        });
-        this.totalResults = response.total;
-        this.aggregations = response.aggregations;
-      },
-      (error) => {
-        console.error('Error fetching data:', error);
-      }
-    );
-  }
-
-  onPageChange(event: PageEvent) {
-    this.pageSize = event.pageSize;
-    this.currentPage = event.pageIndex;
-    this.fetchData();
-  }
-
-  onSearchChange(): void {
-    this.currentPage = 0;
-    this.fetchData();
-  }
-
-  onFilterClick(filterKey: string, filterValue: string): void {
-    // @ts-ignore
-    const index = this.filters[filterKey].indexOf(filterValue);
-    if (index > -1) {
-      // @ts-ignore
-      this.filters[filterKey].splice(index, 1)
-    } else {
-      // @ts-ignore
-      this.filters[filterKey].push(filterValue);
-    }
-    this.fetchData();
-  }
 }
