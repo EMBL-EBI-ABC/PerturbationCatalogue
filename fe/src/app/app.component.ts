8b4a421b28f2ef7a9b20e0a56a98d38fbe62749a
Alexey Sokolov <alexey@ebi.ac.uk>
2025-01-02 11:02:34 +0000

    Adding custom styles, header and footer

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {MatListItem, MatNavList} from "@angular/material/list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+    RouterOutlet,
+    MatListItem,
+    MatNavList,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'fe';
+export class AppComponent implements OnInit {
+  showSidebar = false;
+  constructor(private responsive: BreakpointObserver) {
+  }
+
+  ngOnInit() {
+    this.responsive.observe([
+      Breakpoints.TabletPortrait,
+      Breakpoints.TabletLandscape,
+      Breakpoints.HandsetPortrait,
+      Breakpoints.HandsetLandscape
+    ])
+    .subscribe(result => {
+      this.showSidebar = false;
+      const breakpoints = result.breakpoints;
+      if (
+        breakpoints[Breakpoints.TabletPortrait] ||
+        breakpoints[Breakpoints.TabletLandscape] ||
+        breakpoints[Breakpoints.HandsetPortrait] ||
+        breakpoints[Breakpoints.HandsetLandscape]
+      ) {
+        this.showSidebar = true;
+      }
+    });
+  }
 }
--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {DataPortalComponent} from "./data-portal/data-portal.component";
+import {DetailsComponent} from "./data-portal/details/details.component";
+import {DashboardsComponent} from "./dashboards/dashboards.component";
+import {DataAnalyticsComponent} from "./data-analytics/data-analytics.component";
+import {AboutComponent} from "./about/about.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', title: 'Perturbation Catalogue | Home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'data_portal', title: 'Perturbation Catalogue | Data Portal', component: DataPortalComponent },
+  { path: 'data_portal/:urn', title: 'Perturbation Catalogue | Details', component: DetailsComponent },
+  { path: 'dashboards', title: 'Perturbation Catalogue | Dashboards', component: DashboardsComponent },
+  { path: 'data-analytics', title: 'Perturbation Catalogue | Data Analytics', component: DataAnalyticsComponent },
+  { path: 'about', title: 'Perturbation Catalogue | About', component: AboutComponent },
+];
