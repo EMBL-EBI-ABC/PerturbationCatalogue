e9182b34a067c6234382526ba31ea046176cd12b
Alexey Sokolov <alexey@ebi.ac.uk>
2025-01-02 11:50:42 +0000

    Adding home page

--- fe/src/app/home/home.component.ts
+++ fe/src/app/home/home.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  fontClass: string = "normal_font";
+  cols: number = 3;
+  rowHeight: string = "275px";
+
+  constructor(private responsive: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.responsive.observe([
+      Breakpoints.TabletPortrait,
+      Breakpoints.TabletLandscape,
+      Breakpoints.HandsetPortrait,
+      Breakpoints.HandsetLandscape
+    ])
+      .subscribe(result => {
+        this.fontClass = "normal_font";
+        this.cols = 3;
+        const breakpoints = result.breakpoints;
+        if (
+          breakpoints[Breakpoints.TabletPortrait] ||
+          breakpoints[Breakpoints.TabletLandscape] ||
+          breakpoints[Breakpoints.HandsetPortrait] ||
+          breakpoints[Breakpoints.HandsetLandscape]
+        ) {
+          this.fontClass = "small_font";
+          this.cols = 1;
+        }
+      });
+  }
 
 }
