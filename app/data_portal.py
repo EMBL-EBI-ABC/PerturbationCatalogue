b3564901dcc36c9bb9ef13d22c6b0d294c2fb8ac
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2025-02-19 13:00:24 +0000

    Move details module into elastic_table

--- app/data_portal.py
+++ app/data_portal.py
@@ -5,6 +5,8 @@ from dash import dcc, html, Input, Output, State
 import dash_bootstrap_components as dbc
 import requests
 
+from elastic_table import details
+
 FilterField = namedtuple("FilterField", ["id", "title"])
 filter_fields = [
     FilterField(id="sequenceType", title="Sequence Type"),
@@ -151,105 +153,6 @@ def create_sort_header(column_name, field_name, current_sort):
             },
         ),
         id={"type": "sort-header-container", "field": field_name},
-    )
-
-
-def details_layout(urn):
-    api_url = f"https://perturbation-catalogue-be-959149465821.europe-west2.run.app/mavedb/search/{urn}"
-    response = requests.get(api_url)
-
-    if response.status_code != 200:
-        return html.Div(
-            "Error: Unable to fetch data from the API.", className="alert alert-danger"
-        )
-
-    data = response.json().get("results", [{}])[0]
-
-    return html.Div(
-        [
-            html.Div(
-                [
-                    html.Div(
-                        [
-                            html.Div(
-                                [
-                                    html.H4(
-                                        data.get("title", "N/A"), className="card-title"
-                                    ),
-                                    html.P(
-                                        data.get("shortDescription", "N/A"),
-                                        className="card-text",
-                                    ),
-                                    html.Div(
-                                        [
-                                            html.A(
-                                                "View on MaveDB",
-                                                href=f"https://www.mavedb.org/score-sets/{urn}/",
-                                                className="btn btn-primary mb-3",
-                                            )
-                                        ]
-                                    ),
-                                    html.P(
-                                        [html.Strong("URN: "), data.get("urn", "N/A")],
-                                        className="card-text",
-                                    ),
-                                    html.P(
-                                        [
-                                            html.Strong("Sequence Type: "),
-                                            data.get("sequenceType", "N/A"),
-                                        ],
-                                        className="card-text",
-                                    ),
-                                    html.P(
-                                        [
-                                            html.Strong("Gene Name: "),
-                                            data.get("geneName", "N/A"),
-                                        ],
-                                        className="card-text",
-                                    ),
-                                    html.P(
-                                        [
-                                            html.Strong("Gene Category: "),
-                                            data.get("geneCategory", "N/A"),
-                                        ],
-                                        className="card-text",
-                                    ),
-                                    html.P(
-                                        [
-                                            html.Strong("Publication: "),
-                                            html.A(
-                                                data.get("publicationUrl", "N/A"),
-                                                href=data.get("publicationUrl", "#"),
-                                                target="_blank",
-                                            ),
-                                        ],
-                                        className="card-text",
-                                    ),
-                                    html.P(
-                                        [
-                                            html.Strong("Publication Year: "),
-                                            str(data.get("publicationYear", "N/A")),
-                                        ],
-                                        className="card-text",
-                                    ),
-                                    html.P(
-                                        [
-                                            html.Strong("Number of Variants: "),
-                                            str(data.get("numVariants", "N/A")),
-                                        ],
-                                        className="card-text",
-                                    ),
-                                ],
-                                className="card-body",
-                            )
-                        ],
-                        className="card",
-                    )
-                ],
-                className="col-md-8 mx-auto",
-            )
-        ],
-        className="container mt-4",
     )
 
 
@@ -414,6 +317,6 @@ def register_callbacks(app):
 def resolver(url_parts):
     # If there's something after the main URL, return details page.
     if len(url_parts) == 1:
-        return details_layout(url_parts[0])
+        return details.layout(url_parts[0])
     # Otherwise, return the main data portal page.
     return main_layout
