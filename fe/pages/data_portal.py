a0e74272dfc8e681333c411886048fbe7c5a00e4
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2025-02-27 19:09:31 +0000

    Further simplify column definitions

--- fe/pages/data_portal.py
+++ fe/pages/data_portal.py
@@ -1,72 +1,68 @@
-from collections import namedtuple
-
 import dash
 
 from .elastic_table import ElasticTable, Column
 
 # MaveDB.
 
-columns = [
-    # Special columns: title and subtitle, for details view only.
-    Column(
-        field_name="title",
-        display_table=False,
-        display_details="title",
-    ),
-    Column(
-        field_name="shortDescription",
-        display_table=False,
-        display_details="subtitle",
-    ),
-    # Special column: URN, to serve as the primary key and link construction.
-    Column(
-        field_name="urn",
-        display_name="URN",
-        display_details="text",
-    ),
-    # Data columns.
-    Column(
-        field_name="sequenceType",
-        display_name="Sequence Type",
-        display_details="text",
-        filterable=True,
-    ),
-    Column(
-        field_name="geneName",
-        display_name="Gene Name",
-        display_details="text",
-    ),
-    Column(
-        field_name="geneCategory",
-        display_name="Gene Category",
-        display_details="text",
-        filterable=True,
-    ),
-    Column(
-        field_name="publicationUrl",
-        display_name="Publication URL",
-        display_table=False,
-        display_details="link",
-    ),
-    Column(
-        field_name="publicationYear",
-        display_name="Publication Year",
-        display_details="text",
-        filterable=True,
-        sortable=True,
-        default_sort="desc",
-    ),
-    Column(
-        field_name="numVariants",
-        display_name="Number of Variants",
-        display_details="text",
-        sortable=True,
-    ),
-]
-
 mavedb_table = ElasticTable(
     api_endpoint="https://perturbation-catalogue-be-959149465821.europe-west2.run.app/mavedb/search",
-    columns=columns,
+    columns=[
+        # Special columns: title and subtitle, for details view only.
+        Column(
+            field_name="title",
+            display_table=False,
+            display_details="title",
+        ),
+        Column(
+            field_name="shortDescription",
+            display_table=False,
+            display_details="subtitle",
+        ),
+        # Special column: URN, to serve as the primary key and link construction.
+        Column(
+            field_name="urn",
+            display_name="URN",
+            display_details="text",
+        ),
+        # Data columns.
+        Column(
+            field_name="sequenceType",
+            display_name="Sequence Type",
+            display_details="text",
+            filterable=True,
+        ),
+        Column(
+            field_name="geneName",
+            display_name="Gene Name",
+            display_details="text",
+        ),
+        Column(
+            field_name="geneCategory",
+            display_name="Gene Category",
+            display_details="text",
+            filterable=True,
+        ),
+        Column(
+            field_name="publicationUrl",
+            display_name="Publication URL",
+            display_table=False,
+            display_details="link",
+        ),
+        Column(
+            field_name="publicationYear",
+            display_name="Publication Year",
+            display_details="text",
+            filterable=True,
+            sortable=True,
+            default_sort="desc",
+        ),
+        Column(
+            field_name="numVariants",
+            display_name="Number of Variants",
+            display_details="text",
+            sortable=True,
+        ),
+    ],
     details_button_name="View on MaveDB",
     details_button_link=lambda urn: f"https://www.mavedb.org/score-sets/{urn}/",
 )
