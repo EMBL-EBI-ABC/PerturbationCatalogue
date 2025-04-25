04373618ad9239a7dbb99e2d99ae2b0ebea01dd5
AleksZakirov <45701243+AleksZakirov@users.noreply.github.com>
2025-04-25 10:23:08 +0100

    Remove gene validation for perturbed_target_* fields since 2+ guide experiments have these values separated by |. This should be ok, since validation happens at the curation step anyway.

--- data_exploration/Perturbseq/curation_notebooks/tools/perturbseq_anndata_schema.py
+++ data_exploration/Perturbseq/curation_notebooks/tools/perturbseq_anndata_schema.py
@@ -1,46 +1,40 @@
+import pandas as pd
 from pandera import Field, DataFrameModel
 from pandera.typing import Series, Index
 from typing import Optional
 from pathlib import Path
-import pandas as pd
 
 # Get the absolute path to the current module file
 module_path = Path(__file__).resolve()
 
 # Navigate up to the project root and then to 'ontologies'
-ont_dir = module_path.parent.parent.parent / 'ontologies'
+ont_dir = module_path.parent.parent.parent / "ontologies"
 
-gene_ont = pd.read_parquet(ont_dir / 'genes.parquet').drop_duplicates()
-ctype_ont = pd.read_parquet(ont_dir / 'cell_types.parquet').drop_duplicates()
-cline_ont = pd.read_parquet(ont_dir / 'cell_lines.parquet').drop_duplicates()
-tis_ont = pd.read_parquet(ont_dir / 'tissues.parquet').drop_duplicates()
-dis_ont = pd.read_parquet(ont_dir / 'diseases.parquet').drop_duplicates()
+gene_ont = pd.read_parquet(ont_dir / "genes.parquet").drop_duplicates()
+ctype_ont = pd.read_parquet(ont_dir / "cell_types.parquet").drop_duplicates()
+cline_ont = pd.read_parquet(ont_dir / "cell_lines.parquet").drop_duplicates()
+tis_ont = pd.read_parquet(ont_dir / "tissues.parquet").drop_duplicates()
+dis_ont = pd.read_parquet(ont_dir / "diseases.parquet").drop_duplicates()
 
 
 # adata.obs schema
 class ObsSchema(DataFrameModel):
     perturbation_name: Series[str] = Field(nullable=False)
     perturbed_target_number: Series[int] = Field(nullable=False, ge=1)
-    perturbed_target_ensg: Series[str] = Field(
-        nullable=False, isin=gene_ont.ensembl_gene_id.values
-    )
-    perturbed_target_symbol: Optional[Series[str]] = Field(
-        nullable=True, isin=gene_ont.symbol.values
-    )
-    perturbed_target_category: Optional[Series[str]] = Field(
-        nullable=True, isin=gene_ont.biotype.values
-    )
+    perturbed_target_ensg: Series[str] = Field(nullable=False)
+    perturbed_target_symbol: Optional[Series[str]] = Field(nullable=True)
+    perturbed_target_category: Optional[Series[str]] = Field(nullable=True)
     perturbation_type_label: Series[str] = Field(nullable=False)
     perturbation_type_id: Series[str] = Field(nullable=True, str_contains=":")
-    timepoint: Optional[Series[str]] = Field(nullable=True, regex = r"^P\d+DT\d{1,2}H\d{1,2}M\d{1,2}S$") # /^P\d+DT\d{1,2}H\d{1,2}M\d{1,2}S$/gm
+    timepoint: Optional[Series[str]] = Field(
+        nullable=True, regex=r"^P\d+DT\d{1,2}H\d{1,2}M\d{1,2}S$"
+    )
     treatment_label: Optional[Series[str]] = Field(nullable=True)
     treatment_id: Optional[Series[str]] = Field(nullable=True, str_contains=":")
     model_system_label: Series[str] = Field(nullable=False)
     model_system_id: Series[str] = Field(nullable=True, str_contains=":")
     species: Series[str] = Field(nullable=False, isin=["Homo sapiens"])
-    tissue_label: Optional[Series[str]] = Field(
-        nullable=True, isin=tis_ont.name.values
-    )
+    tissue_label: Optional[Series[str]] = Field(nullable=True, isin=tis_ont.name.values)
     tissue_id: Optional[Series[str]] = Field(
         nullable=True,
         isin=tis_ont.ontology_id.values,
@@ -60,7 +54,9 @@ class ObsSchema(DataFrameModel):
     sex_label: Optional[Series[str]] = Field(nullable=True)
     sex_id: Optional[Series[str]] = Field(nullable=True, str_contains=":")
     developmental_stage_label: Optional[Series[str]] = Field(nullable=True)
-    developmental_stage_id: Optional[Series[str]] = Field(nullable=True, str_contains=":")
+    developmental_stage_id: Optional[Series[str]] = Field(
+        nullable=True, str_contains=":"
+    )
     disease_term_label: Optional[Series[str]] = Field(
         nullable=True, isin=dis_ont.name.values
     )
@@ -80,19 +76,19 @@ class VarSchema(DataFrameModel):
     ensembl_gene_id: Index[str] = Field(
         nullable=False,
         unique=True,
-        str_startswith=("ENSG", 'control'),
+        str_startswith=("ENSG", "control"),
         # isin=gene_ont.ensembl_gene_id.values,
         check_name=True,
     )
     ensembl_gene_id: Series[str] = Field(
-        nullable=False, 
+        nullable=False,
         unique=True,
-        str_startswith=("ENSG", 'control'),
+        str_startswith=("ENSG", "control"),
         # isin=gene_ont.ensembl_gene_id.values
     )
     symbol: Series[str] = Field(
         nullable=True,
-        coerce=True
+        coerce=True,
         # isin=gene_ont.symbol.values
     )
 
