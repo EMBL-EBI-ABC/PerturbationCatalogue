9646e1a079d9953c5077b1f4b2cf45aa0afe8e78
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2026-01-06 19:16:46 +0000

    Simplify mave_data table construction

--- dwh/bq_dbt/models/mave_data.sql
+++ dwh/bq_dbt/models/mave_data.sql
@@ -10,23 +10,12 @@
     )
 }}
 
-with
-    meta as (
-        select dataset_id, sample_id, perturbed_target_symbol, ingested_at
-        from {{ source('mave', 'metadata') }}
-    ),
-    data as (
-        select dataset_id, sample_id, score_name, score_value, ingested_at
-        from {{ source('mave', 'data') }}
-    )
 select
-    d.dataset_id,
-    d.sample_id,
-    m.perturbed_target_symbol,
-    d.score_name,
-    d.score_value,
-    greatest(d.ingested_at, m.ingested_at) as max_ingested_at
-from data d
-join meta m
-    on d.dataset_id = m.dataset_id and d.sample_id = m.sample_id
+    dataset_id,
+    sample_id,
+    perturbed_target_symbol,
+    score_name,
+    score_value,
+    ingested_at as max_ingested_at
+from {{ source('mave', 'data') }}
 
