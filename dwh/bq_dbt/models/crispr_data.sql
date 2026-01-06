84a1f8ffaec9541ad6cfdf5fcfe278d9f884cad9
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2026-01-06 19:16:46 +0000

    Simplify crispr_data table construction

--- dwh/bq_dbt/models/crispr_data.sql
+++ dwh/bq_dbt/models/crispr_data.sql
@@ -10,25 +10,14 @@
     )
 }}
 
-with
-    meta as (
-        select dataset_id, sample_id, perturbed_target_symbol, significant, significance_criteria, ingested_at
-        from {{ source('crispr', 'metadata') }}
-    ),
-    data as (
-        select dataset_id, sample_id, score_name, score_value, ingested_at
-        from {{ source('crispr', 'data') }}
-    )
 select
-    d.dataset_id,
-    d.sample_id,
-    m.perturbed_target_symbol,
-    d.score_name,
-    d.score_value,
-    m.significant,
-    m.significance_criteria,
-    greatest(d.ingested_at, m.ingested_at) as max_ingested_at
-from data d
-join meta m
-    on d.dataset_id = m.dataset_id and d.sample_id = m.sample_id
+    dataset_id,
+    sample_id,
+    perturbed_target_symbol,
+    score_name,
+    score_value,
+    significant,
+    significance_criteria,
+    ingested_at as max_ingested_at
+from {{ source('crispr', 'data') }}
 
