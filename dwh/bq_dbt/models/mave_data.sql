d3eed5cabbefac8cac6e797275b527e23f4bb6c4
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2026-01-08 17:38:03 +0000

    Extract MAVE perturbation position, wild type and change

--- dwh/bq_dbt/models/mave_data.sql
+++ dwh/bq_dbt/models/mave_data.sql
@@ -16,6 +16,10 @@ select
     perturbed_target_symbol,
     score_name,
     score_value,
+    perturbation_name,
+    cast(regexp_extract(perturbation_name, r'p\.[a-zA-Z]+(\d+)') as int64) as perturbation_position,
+    regexp_extract(perturbation_name, r'p\.([a-zA-Z]+)\d+') as perturbation_aa_wt,
+    regexp_extract(perturbation_name, r'p\.[a-zA-Z]+\d+([a-zA-Z=]+)') as perturbation_aa_change,
     ingested_at as max_ingested_at
 from {{ source('mave', 'data') }}
 
