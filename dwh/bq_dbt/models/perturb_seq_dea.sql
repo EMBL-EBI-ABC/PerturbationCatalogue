298deea7b1f00ddd62b3dcd6f4980e7af3caba0b
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2026-01-21 13:13:23 +0000

    Add new DBT model for perturb_seq_dea

---
+++ dwh/bq_dbt/models/perturb_seq_dea.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized="table",
+        partition_by={
+            "field": "max_ingested_at",
+            "data_type": "timestamp",
+            "granularity": "day",
+        },
+        cluster_by=["dataset_id", "perturbed_target_symbol"],
+    )
+}}
+
+select
+    dataset_id,
+    perturbed_target_symbol,
+    gene,
+    padj,
+    log2FoldChange as log2foldchange,
+    score_name,
+    score_value,
+    cell_type,
+    current_timestamp() as max_ingested_at
+from {{ source("perturb_seq", "pertpy_dea") }}
