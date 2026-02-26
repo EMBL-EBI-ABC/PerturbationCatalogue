494be3944831d2eeea1bf4b1ae1ad1e131f5d3ab
Aleks Zakirov <zakirov@ebi.ac.uk>
2026-02-26 16:04:20 +0000

    Update developmental_stage_label description to include age group definitions

--- data_exploration/curation_tools/perturbseq_anndata_schema.py
+++ data_exploration/curation_tools/perturbseq_anndata_schema.py
@@ -170,7 +170,7 @@ class ObsSchema(DataFrameModel):
     )
     developmental_stage_label: Series[String] = Field(
         nullable=True,
-        description="Developmental stage ontology term label of the investigated sample.",
+        description="Developmental stage ontology term label of the investigated sample. The age groups are defined as follows: embryonic (conception to 8 weeks), fetal (9 weeks to birth), child (0-12 years), adolescent (13-18 years), adult (19-59 years), senior adult (60 years and above).",
         isin=["embryonic", "fetal", "neonatal", "child", "adolescent", "adult", "senior adult"],
     )
     developmental_stage_id: Series[String] = Field(
