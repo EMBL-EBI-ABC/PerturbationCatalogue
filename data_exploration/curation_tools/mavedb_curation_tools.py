17f8e1347d05ce6d48d37ae23223c1f6c1a15247
AleksZakirov <45701243+AleksZakirov@users.noreply.github.com>
2026-06-11 10:58:16 +0100

    Remove default separator in curate_mavedb function

--- data_exploration/curation_tools/mavedb_curation_tools.py
+++ data_exploration/curation_tools/mavedb_curation_tools.py
@@ -449,7 +449,6 @@ def curate_mavedb(
         slot="obs",
         input_column="perturbed_target_symbol",
         count_column_name="perturbed_target_number",
-        sep="|",
     )
 
     # add chromosome encoding
